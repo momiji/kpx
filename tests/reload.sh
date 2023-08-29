da85021967fac1467017316b7cfb16b932d9d0b8
momiji <momiji@github.com>
2023-08-29 17:37:17 +0200

    tests: add scripts

---
+++ tests/reload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -Eeuo pipefail
+cd "$(dirname "$0")"
+
+cd ..
+make fast
+docker compose -f ./tests/kpx/docker-compose.yaml restart client -t 0
+
+max=2
+while (( $max > 0 )) ; do
+  nc 127.0.0.1 8888 -vz &> /dev/null && break
+  sleep 0.5
+done
+
+exit 0
